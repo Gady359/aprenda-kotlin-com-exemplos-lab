e37f122f3e6af8b77f308a6f90565d07793ab0cb
Gabriel Peters <115185950+Gady359@users.noreply.github.com>
2024-01-22 18:55:48 -0300

    Projeto feito

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,57 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(
+    val nome : String,
+    val email : String,
+    val idade : Int
+)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(
+    var nome: String, 
+    val duracao: Int = 60,
+    val descricao : String,
+    val nivel : Nivel =Nivel.BASICO
+)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String) {
 
-    val inscritos = mutableListOf<Usuario>()
+    private val conteudos = mutableListOf<ConteudoEducacional>()
+    private val inscritos = mutableListOf<Usuario>()
+    
+    fun adicionarConteudo(conteudo: ConteudoEducacional) {
+        conteudos.add(conteudo)
+    }
+
+    fun removerConteudo(conteudo: ConteudoEducacional) {
+        conteudos.remove(conteudo)
+    }
+    
+     fun exibirInformacoes() {
+        println("Formação: $nome")
+        println("Conteúdos:")
+        conteudos.forEach { println("${it.nome} - ${it.nivel}") }
+        println("Inscritos: ${inscritos.size}")
+    }
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("João", "joao@email.com", 25)
+    val usuario2 = Usuario("Maria", "maria@email.com", 30)
+
+    val conteudo1 = ConteudoEducacional("Introdução à Programação", 90, "Fundamentos de programação", Nivel.BASICO)
+    val conteudo2 = ConteudoEducacional("Banco de Dados Avançado", 120, "Modelagem e otimização de bancos de dados", Nivel.DIFICIL)
+
+    val formacao1 = Formacao("Desenvolvedor Web Full Stack")
+    formacao1.adicionarConteudo(conteudo1)
+    formacao1.adicionarConteudo(conteudo2)
+
+    formacao1.matricular(usuario1)
+    formacao1.matricular(usuario2)
+
+    formacao1.exibirInformacoes()
+
 }
